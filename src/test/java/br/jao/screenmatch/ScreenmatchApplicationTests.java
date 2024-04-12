3c76e4551ec5f7848d7487672b171ee332ca800f
joaoVictor <jvdcorrea@gmail.com>
2024-04-12 15:10:57 -0300

    first commit

---
+++ src/test/java/br/jao/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jao.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
